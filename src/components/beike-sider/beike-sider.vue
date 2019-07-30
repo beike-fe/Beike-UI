6b0f6d6ddee165e2ed6dcefb8d2e299f73beef0c
谭源杰 <tanyuanjie@tanyuanjiedeMacBook-Pro.local>
2019-07-30 16:49:34 +0800

    修改了beike-sider的一些代码格式

--- src/components/beike-sider/beike-sider.vue
+++ src/components/beike-sider/beike-sider.vue
@@ -2,21 +2,13 @@
     <div class="sidebar">
         <div class="sidebar-item">
             <div>
-                <button class="guide-open" @click="function(){
-                $emit('change');
-                expend=false;
-                table=true;
-                }" v-if="expend">→</button>
-                <button class="guide" @click="function(){
-                $emit('change');
-                expend=true;
-                table=false;
-                }" v-if="!expend">关闭</button>
+                <button class="guide-open" @click="handleOpen" v-if="expend">→</button>
+                <button class="guide" @click="handleClose" v-else>关闭</button>
             </div>
             <div class="isShow" v-if="table">
                 <li class="guide-li">导航</li>
                 <div v-for="item in info">
-                    <button  @click="jump(item)" class="guide">{{item.name}}</button>
+                    <button @click="jump(item)" class="guide">{{item.name}}</button>
                 </div>
             </div>
 
@@ -25,57 +17,78 @@
 </template>
 
 <script>
-    import BeikeButton from "../beike-button/beike-button"
+    import BeikeButton from '../beike-button/beike-button';
+
     export default {
-        name: "beike-sider",
-        components: {BeikeButton},
-        props:{
-            isShow:{
-                type:Boolean,
-                default:false,
+        name: 'beike-sider',
+        components: { BeikeButton },
+        props: {
+            isShow: {
+                type: Boolean,
+                default: false,
             },
-            titleShow:{
-                type:Boolean,
-                default:true,
+            titleShow: {
+                type: Boolean,
+                default: true,
             },
-            isRectangle:{
-                type:Boolean,
-                default:false,
-            }
+            isRectangle: {
+                type: Boolean,
+                default: false,
+            },
         },
-        data () {
+        data() {
             return {
                 info: [
-                    {name:"button",
-                        path:"/beike-button"},
-                    {name:"sider",
-                        path:"/beike-sider"},
-                    {name:"topmenu",
-                        path:""},
-                    {name:"navigate",
-                        path:"/navigate"},
-                    {name:"tip",
-                        path:"/tip"},
-                    {name:"index",
-                        path:"/"},
+                    {
+                        name: 'button',
+                        path: '/beike-button',
+                    },
+                    {
+                        name: 'sider',
+                        path: '/beike-sider',
+                    },
+                    {
+                        name: 'topmenu',
+                        path: '',
+                    },
+                    {
+                        name: 'navigate',
+                        path: '/navigate',
+                    },
+                    {
+                        name: 'tip',
+                        path: '/tip',
+                    },
+                    {
+                        name: 'index',
+                        path: '/',
+                    },
                 ],
                 expend: true,
                 table: false,
                 item: '',
-            }
+            };
         },
-        mounted () {
+        mounted() {
 
         },
         methods: {
-        jump:function (item) {
-            let _path = item.path;
-            console.log(_path),
-            this.$router.push(
-                {path:_path})
-        }
+            jump: function ( item ) {
+                let _path = item.path;
+                this.$router.push({ path: _path });
+            },
+            handleOpen: function () {
+                this.$emit('change');
+                this.expend = false;
+                this.table = true;
+            },
+            handleClose: function () {
+                this.$emit('change');
+                this.expend = true;
+                this.table = false;
+            },
         },
-    }
+    };
 </script>
 
 <style scoped>
